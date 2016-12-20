f79de4279c985494371b4468f20891ea26611514
Geoffrey, Tang <jeffswt@outlook.com>
2016-12-21 00:13:42 +0800

    Implemented compression and pickling for local storage manager.

--- storage.py
+++ storage.py
@@ -1,5 +1,44 @@
 
+import pickle
 import copy
+import lzma
+
+__storage_index = None
+
+def load_storage(file_name='pbio4.context'):
+    """ load_storage() -- Load storage from local file. Default to file name
+    "pbio4.context". """
+    global __storage_index
+    # Reading file.
+    store = {}
+    try:
+        f = open(file_name, 'rb')
+        d = f.read()
+        f.close()
+        # De-compressing data.
+        d = lzma.decompress(d)
+        # Un-pickling data
+        store = pickle.loads(d)
+    except Exception as err:
+        store = {}
+    if type(store) != dict:
+        store = {}
+    __storage_index = store
+    return
+
+def flush_storage(file_name='pbio4.context'):
+    """ flush_storage() -- Flush storage to local file. Default to file name
+    "pbio4.context". """
+    # Pickling data.
+    d = pickle.dumps(__storage_index)
+    # Compressing data.
+    d = lzma.compress(d)
+    # Writing to file.
+    f = open(file_name, 'wb')
+    f.seek(0)
+    f.write(d)
+    f.close()
+    return
 
 def get(engine_id):
     """ get(engine_id) -- Retrieve local storage for given engine. """
